4415af99caa59d369e09fe9495ca3fb8107ac2a4
radeksvarz <radek.svarz@gmail.com>
2016-01-11 01:59:32 +0100

    reqs a settings update

---
+++ settings/dev.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Local dev settings and globals.
+"""
+
+from settings.base import *
+
+
+########## DEBUG
+DEBUG = True
+# depreceated - https://docs.djangoproject.com/en/1.8/ref/settings/#template-debug
+# set by DEBUG - https://docs.djangoproject.com/en/1.8/topics/templates/#django.template.backends.django.DjangoTemplates
+# TEMPLATE_DEBUG = DEBUG
+# SERVE_MEDIA = DEBUG
+
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
