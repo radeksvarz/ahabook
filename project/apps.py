1aff264bbbfc6cf91a8832c887022ae60275d62d
radeksvarz <radek.svarz@gmail.com>
2016-01-23 21:09:35 +0100

    autoset sites in DB from settings - fix run b4 migration

--- project/apps.py
+++ project/apps.py
@@ -2,6 +2,9 @@ from django.apps import AppConfig
 
 from django.contrib import sites
 from django.conf import settings
+from django.apps import apps
+from django.db.utils import ProgrammingError
+
 
 class ProjectConfig(AppConfig):
     name = 'project'
@@ -11,11 +14,20 @@ class ProjectConfig(AppConfig):
         if hasattr(self, "ready_run_once"): return
         self.ready_run_once = True
 
+        try:
+            Site = apps.get_model('sites', 'Site')
+        except LookupError:
+            return
+
         conf_sites = getattr(settings, "SITES", [("example.com", "Example"), ])
+
+        try:
+            Site.objects.all().delete()
+        except ProgrammingError:
+            print("Sites configuration has an unapplied migration. Sites SETTINGS will not be used until the migration is executed.")
+            return
 
         print("Setting sites in DB to:", conf_sites)
 
-        sites.models.Site.objects.all().delete()
-
         for id, conf_site in enumerate(conf_sites, 1):
-            sites.models.Site.objects.create(id = id, domain = conf_site[0], name = conf_site[1])
+            Site.objects.create(id=id, domain=conf_site[0], name=conf_site[1])
